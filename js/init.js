76d59d515ba3c5c36ef161b28d4d36f96425d9eb
Rennan <rennandamiao@gmail.com>
2021-03-12 14:53:54 -0300

    atualização Portifólios e Formulário de Contrato

--- js/init.js
+++ js/init.js
@@ -5,3 +5,7 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options);
+});
