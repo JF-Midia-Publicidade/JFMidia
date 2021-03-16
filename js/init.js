8b8c8d1ec723ef2b7f4c8222d839b98db144c8ce
Rennan <rennandamiao@gmail.com>
2021-03-16 13:54:32 -0300

    novoEstilo

--- js/init.js
+++ js/init.js
@@ -11,3 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 $(".dropdown-trigger").dropdown();
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+
